7a13f4366d5753a3d1b401a33261482fe92a7307
Jay Sullivan <jay@petio.org>
2012-12-26 22:27:26 -0500

    trybloop - refactor trybloop.c

--- trybloop.c
+++ trybloop.c
@@ -4,43 +4,51 @@
 #define SAMPLE_RATE 44100
 #define FRAMES_PER_BUFFER 512
 
-struct StreamCallbackData
+/* describes all of the characteristics of a playable note */
+typedef struct trybloop_note
 {
-  int frame;
   float frequency;
   float amplitude;
-} StreamCallbackData;
+  float duration;
+  /* trybloop_waveform waveform; */
+} trybloop_note;
 
+/* describes an actively playing note */
+typedef struct trybloop_note_active 
+{
+  trybloop_note note;
+  int frame;
+} trybloop_note_active;
 
-static int streamCallback(
+/* gets called to fill a sound device's buffer with audio samples (frames) */
+/* our buffer consists of 512 frames, and each frame is a float32 */
+static int trybloop_note_active_callback(
 	const void *inputBuffer,
 	void *outputBuffer, 
 	unsigned long framesPerBuffer,
 	const PaStreamCallbackTimeInfo* timeInfo, 
 	PaStreamCallbackFlags statusFlags,
-	void *inData)
+	void *data)
 { 
-       float *buffer = (float*)outputBuffer; 
-       struct StreamCallbackData *data = (struct StreamCallbackData*)inData;
+       /* get properties of the active note */
+       trybloop_note_active* activeNote = (trybloop_note_active*)(data);
+       float frequency = activeNote->note.frequency;
+       float amplitude = activeNote->note.amplitude;
 
-       float frequency = data->frequency;
-       float amplitude = data->amplitude;
-              
+       /*  draw a sine wave in buffer using the properties of the active note */
+       float *buffer = (float*)outputBuffer; 
        for(int frame = 0; frame < framesPerBuffer; frame++)
        {
-           float totalFrames = (float)(data->frame++);
-           buffer[frame] = amplitude * 
-                sin( frequency * 2 * M_PI * totalFrames / SAMPLE_RATE);
+           buffer[frame] = amplitude *
+                sin( frequency * 2 * M_PI * activeNote->frame / SAMPLE_RATE);
+//printf("freq %f amp %f frame %d buffer %f\n", frequency,amplitude,activeNote->frame,buffer[frame]);
+           activeNote->frame++;
        }
        return paContinue; /* 0 == continue */
 }
 
-static void PlayNote(float frequency, float amplitude, float duration)
+static void trybloop_note_play(trybloop_note_active* activenote)
 {
-   // create an object with the note data
-   struct StreamCallbackData data = {0};
-   data.frequency = frequency;
-   data.amplitude = amplitude;
 
    // create stream
    PaStream * stream;
@@ -50,14 +58,12 @@ static void PlayNote(float frequency, float amplitude, float duration)
         paFloat32,        /* sample format */
         SAMPLE_RATE,      /* sample rate */
         FRAMES_PER_BUFFER,/* frames per buffer */
-        streamCallback,   /* user-defined callback function */
-        &data             /* user-defined callback argument */
+        trybloop_note_active_callback,   /* user-defined callback function */
+        activenote       /* user-defined callback argument */
    );
 
    // start the stream
    Pa_StartStream(stream);
-   Pa_Sleep(duration);
-   Pa_StopStream(stream);
 }
 
 int main(int argc, char *argv[])
@@ -66,8 +72,18 @@ int main(int argc, char *argv[])
    // setup
    Pa_Initialize();
 
+   // set up a note to play
+   trybloop_note_active active;
+   active.frame = 0;
+   active.note.frequency = 440.0;
+   active.note.amplitude = 1.0;
+   active.note.duration = 10000;
+
    // play the note
-   PlayNote(440.0, 1.0, 1000);
+   trybloop_note_play(&active);
+
+   // wait for the specified duration
+   Pa_Sleep(active.note.duration);
 
     // teardown
    Pa_Terminate();
